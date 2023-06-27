f1dc6d8b99bcf7d8db094a770629fd2f7195070d
gabrielferrazz <gabriel.rferraz@hotmail.com>
2023-06-27 10:09:05 -0300

    atualização Forca

--- desafio_da_forca.py
+++ desafio_da_forca.py
@@ -1,55 +1,33 @@
-# lista = []
-# print(lista)
-
-# lista.append('ola')
-# print(lista)
-
-# lista.append(8)
-# print(lista)
-
-# lista.pop(1)
-# print(lista)
-
-# lista.insert(5, 3)
-# print(lista)
-
-
-
-
 palavra_secreta = 'paralelepipedo'
-lista = ['_']*len(palavra_secreta)
-numero_max_tentativas = len(palavra_secreta)*2
+lista = ['_'] * len(palavra_secreta)
+numero_max_tentativas = len(palavra_secreta) * 2
 tentativa_atual = 0
 
-print('Adivinhe a palavra ', lista)
+print('Adivinhe a palavra:', ' '.join(lista))
 
 while tentativa_atual <= numero_max_tentativas:
-
-    entrada = input('Digite uma letra: ').lower()
+    entrada = input('Digite uma letra: ')
 
     if entrada in lista:
-        print('Ja acertou essa letra.')    
+        print('Você já acertou essa letra.')
     else:
         tentativa_atual += 1
 
-    if entrada in palavra_secreta:
-        print(entrada)
-        posicao = (palavra_secreta.index(entrada))
-        lista [posicao] = entrada
-        print(lista)
+    acertos = 0
+    for i, letra in enumerate(palavra_secreta):
+        if letra == entrada:
+            lista[i] = entrada
+            acertos += 1
 
+    if acertos > 0:
+        print('A palavra:', ' '.join(lista))
     else:
-        print('Essa letra não está na palavra')
+        print('Essa letra não está na palavra.')
 
     if '_' not in lista:
-        print('Você acertou :D ')
+        print('Você acertou a palavra!')
         break
 
-
-
-print(palavra_secreta)
-
-
-
+print('A palavra secreta era:', palavra_secreta)
 
 
