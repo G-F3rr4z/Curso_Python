cbbe5a9e4c070469b8b5dffdd3aa2c303598e3a5
gabrielferrazz <gabriel.rferraz@hotmail.com>
2023-06-26 23:36:27 -0300

    aula 58 a 60

--- aula60.py.py
+++ aula60.py.py
@@ -32,8 +32,9 @@ O primeiro dígito do CPF é 7
 
 # cpf = input('Insira o seu CPF (digite apenas numeros.): ')
 
-cpf = '12482929601'
-nove_digitos = cpf[:9]
+cpf_enviado_cliente = input('Digite o seu CPF, digite somente numeros:  ')
+nove_digitos = cpf_enviado_cliente[:9]
+dez_digitos = cpf_enviado_cliente[:10]
 contador_regressivo_1 = 10
 resultado_1 = 0
 multiplicacao_1 = 0
@@ -46,17 +47,19 @@ for digito_1 in nove_digitos:
     contador_regressivo_1 -= 1
     multiplicacao_1 = resultado_1 * 10
     digito_1 = multiplicacao_1 % 11
-if digito_1 > 9:
-    print('0')
-else:
-    print(digito_1)
 
-for digito_2 in nove_digitos:
+for digito_2 in dez_digitos:
     resultado_2 += int(digito_2) * contador_regressivo_2
     contador_regressivo_2 -= 1
     multiplicacao_2 = resultado_2 * 10
     digito_2 = multiplicacao_2 % 11
-if digito_2 > 9:
-    print('0')
+
+cpf_gerado = f'{nove_digitos}{digito_1}{digito_2}'
+
+if cpf_enviado_cliente == cpf_gerado:
+    print('CPF: ' f'{cpf_enviado_cliente} é Válido')
 else:
-    print(digito_2)
+    print('CPF: ' f'{cpf_enviado_cliente} é Inválido')
+
+
+
