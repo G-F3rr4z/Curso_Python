d2fde18b839aa85c6a46a7333e8a0cb91b6a32c2
gabrielferrazz <gabriel.rferraz@hotmail.com>
2023-07-20 00:07:22 -0300

    intermediario

--- Intermediario/aula82.py
+++ Intermediario/aula82.py
@@ -8,8 +8,8 @@
 # Criando um set
 # set(iterável) ou {1, 2, 3}
 # s1 = set('Luiz')
-s1 = set()  # vazio
-s1 = {'Gabriel', 1, 2, 3}  # com dados
+# # s1 = set()  # vazio
+s1 = set({1, 2, 3, 1, 4, 5, 1 })  # com dados
 print(s1)
 # Sets são eficientes para remover valores duplicados
 # de iteráveis.
@@ -18,8 +18,16 @@ print(s1)
 # - não tem índexes;
 # - não garantem ordem;
 # - são iteráveis (for, in, not in)
+# s1 = { 1, 2, 3}
+# s1 = set()
+# s1.add(1)
+# s1.add('Gabriel')
+# s1.update(('Ola mundo', 1,2,3,4,5,6))
+# s1.discard(6)
 
-# Métodos úteis:
+# print(s1)
+
+# Métodos úteis: 
 # add, update, clear, discard
 
 # Operadores úteis:
@@ -27,3 +35,11 @@ print(s1)
 # intersecção & (intersection) - Itens presentes em ambos
 # diferença - Itens presentes apenas no set da esquerda
 # diferença simétrica ^ - Itens que não estão em ambos
+# s1 = {1,2,3}
+# s2 = {2,4,3}
+# s3 = s1 | s2
+# s3 = s1 & s2
+# s3 = s2 - s1
+# s3 = s2 ^ s1
+# print(s3)
+
