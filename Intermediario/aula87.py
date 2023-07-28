bcbb29e9033ac734b726689da78fa96f9a71be64
gabrielferrazz <gabriel.rferraz@hotmail.com>
2023-07-28 00:42:47 -0300

    intermediario

---
+++ Intermediario/aula87.py
@@ -0,0 +1,24 @@
+# lista = [numero for numero in range(10)]
+# print(lista)
+
+# lista = [numero * 2 for numero in range(10)]
+# print(lista)
+import pprint
+
+produtos = [
+    {'nome': 'p1', 'preco': 20, },
+    {'nome': 'p2', 'preco': 10, },
+    {'nome': 'p3', 'preco': 30, },
+]
+novos_produtos = [
+    {**produto, 'preco': produto['preco'] * 1.05}
+    if produto['preco'] > 20 else {**produto}
+    for produto in produtos
+]
+
+# print(*novos_produtos, sep='\n')
+
+lista = [n for n in range(10) if n < 5]
+print(lista)
+
+
